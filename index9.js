b51d940524ae21d6a2ebcf88281ea53ced405c8e
Harikesh91 <harishkumarkushwaha9118@gmail.com>
2024-06-01 14:50:35 +0530

    l

---
+++ index9.js
@@ -0,0 +1,55 @@
+
+  
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 1287, 3000);
+  counter("count2", 100, 5786, 2500);
+  counter("count3", 0, 1440, 3000);
+  counter("count4", 0, 7110, 3000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+
+})();
